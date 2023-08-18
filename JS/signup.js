2254bd97ef8c151a7475f9a14b5448b29459b433
Cineflix23 <129967762+Cineflix23@users.noreply.github.com>
2023-08-18 11:21:46 -0500

    Add files via upload

---
+++ JS/signup.js
@@ -0,0 +1,41 @@
+
+
+const signUpForm = document.querySelector('#signupForm');
+signUpForm.addEventListener('submit', (e) =>{
+  e.preventDefault()
+  const name = document.querySelector('#name').value 
+  const email = document.querySelector('#email').value 
+  const password = document.querySelector('#password').value 
+  const Users = JSON.parse(localStorage.getItem('users')) || [] 
+  const isUserRegistered = Users.find(user => user.email === email)
+  if(isUserRegistered){
+    return swal.fire('Usuario ya registrado');
+  }
+  Users.push({name:name, email:email, password:password})
+  localStorage.setItem('users', JSON.stringify(Users))
+  swal.fire('Registro Exitoso');
+  window.location.href = 'login.html';
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
